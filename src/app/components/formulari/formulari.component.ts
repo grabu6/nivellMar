eae163d78e5d70f534259cdc97649e4855a3b856
grabu6 <mgrabulosa071@boscdelacoma.cat>
2023-06-08 00:25:26 +0200

    1r commit (projecte creat)

---
+++ src/app/components/formulari/formulari.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulari',
+  templateUrl: './formulari.component.html',
+  styleUrls: ['./formulari.component.css']
+})
+export class FormulariComponent {
+
+}
